119d69d70dbfaed9bf33032910e148f526e0c48d
Jugep001 <juliang1@live.de>
2021-12-26 13:03:06 +0100

    Projektdateien hinzufügen.

---
+++ JokesWebApp/Models/Jokes.cs
@@ -0,0 +1,19 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+
+        public int JokeQuestion { get; set; }
+
+
+        public int JokeAnswer { get; set; }
+
+
+        public Jokes()
+        {
+
+        }
+
+    }
+}
